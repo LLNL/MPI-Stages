c3bd6115905a75c209ba839c0ec6e456603ce50e
Anthony Skjellum <skjellum@gmail.com>
2017-08-13 14:14:26 -0500

    Part 2 of that manual merge.

---
+++ src/inc/mpi_ops.h
@@ -0,0 +1,11 @@
+#ifndef _MPI_OPS_H
+#define _MPI_OPS_H
+
+namespace exampi
+{
+  void internal_MAX_OP(void *invec, void *inoutvec, int *len, MPI_Datatype *dt);
+  void internal_MIN_OP(void *invec, void *inoutvec, int *len, MPI_Datatype *dt);
+  void internal_SUM_OP(void *invec, void *inoutvec, int *len, MPI_Datatype *dt);
+}
+
+#endif
