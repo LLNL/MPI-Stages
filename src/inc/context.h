31f16b44452b9deb5a67518d60caa31ed4f07d84
Tony Skjellum <skjellum@gmail.com>
2017-08-30 16:17:41 -0400

    Add stubs and basic first steps for real groups and communicators... subject to additional refinement.

---
+++ src/inc/context.h
@@ -0,0 +1,20 @@
+#ifndef _EXAMPI_CONTEXT_H
+#define _EXAMPI_CONTEXT_H
+
+#include <Exampi.h>
+
+namespace exampi {
+
+
+class Context
+{
+ public:
+ protected:
+ private:
+
+};
+
+
+}
+
+#endif
---
+++ src/inc/group.h
@@ -0,0 +1,19 @@
+#ifndef _EXAMPI_GROUP_H
+#define _EXAMPI_GROUP_H
+
+#include <Exampi.h>
+
+namespace exampi {
+
+class Group
+{
+ public:
+ protected:
+ private:
+
+};
+
+}
+
+#endif
+
