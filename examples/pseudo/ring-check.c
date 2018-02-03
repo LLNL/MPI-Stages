38add93be31b25a90bf0669a258c071219831bd8
Nawrin <nawrin@3130-mac0s-iMac.local>
2018-02-02 19:57:31 -0600

    Updated checkpoint example

--- examples/pseudo/ring-check.c
+++ examples/pseudo/ring-check.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "mpi.h"
 
 #define ARRAY_LEN 4
@@ -7,107 +8,120 @@
 #define TAG 0
 
 #define DEBUG
+#define MPIX_SAFE_CALL(__operation,__predicate,__label) {int __code = __operation; if(__code != MPI_SUCCESS) {__predicate; goto __label;}}
+
+int Application_Checkpoint_Read();
+int Application_Checkpoint_Write();
+
+int main_loop(int restart_iteration, int *done);
 
 int main(int argc, char** argv)
 {
-  int i, epoch;
-  int process_0_exit = 5;
-  int rank, size;
-  int smallmessage[ARRAY_LEN];
-  MPI_Status status;
+  int abort = 0;
+  int done = 0;
+  int recoveru_code;
+  int code = MPI_SUCCESS;
+  int fault_epoch = 0;
 
-#ifdef DEBUG
-  printf("Entering main...\n");
-#endif
+  while (!abort && !done) {
+	  switch(code) {
+	  case MPI_SUCCESS: //process create/recreate situation
+		  recovery_code = MPIX_Init(&argc, &argv);
+		  MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+		  break;
+	  case MPIX_TRY_RELOAD:
+		  recovery_code = MPIX_LOAD_checkpoint(); //read the second to last checkpointed MPI state for this process and clear pending communication
+		  break;
+	  default:
+		  abort = 1;
+		  break;
+	  }
+	  if (abort)
+		  goto abort_process;
 
-#ifdef DEBUG
-  printf("About to init...\n");
-#endif
-  int recovery-code = MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-#ifdef DEBUG
-  printf("%d: Size=%d, rank=%d\n", rank, size, rank);
-  printf("%d: About to populate smallmessage...\n",rank);
-#endif
-  // MPI loaded it's previous state; now application needs to load
-  if (recovery-code == MPIX_SUCCESS_RECOVERY) {
-	  MPI_Epoch(&epoch);
-	  Application_Checkpiont_load(&epoch, smallmessage);
-	  process_0_exit = 100;
+	  switch(recovery_code) {
+	  case MPI_SUCCESS: //This is the first time we started a process
+		  code = main_loop(fault_epoch, &done);
+		  break;
+	  case MPIX_SUCCESS_RECOVERY: //We didn't restart the process, we reloaded MPI checkpoint only
+		  MPIX_GET_fault_epoch(&fault_epoch);
+		  code = main_loop(fault_epoch, &done);
+		  break;
+	  case MPIX_SUCCESS_RESTART: //For restarted process
+		  MPIX_Get_fault_epoch(&fault_epoch);
+		  MPIX_Load_checkpoint();
+		  code = main_loop(fault_epoch, &done);
+		  break;
+	  default:
+		  abort = 1;
+		  break;
+	  }
+	  abort_process:
+	  if (abort) {
+		  //MPI_Abort(MPI_COMM_WORLD);
+	  }
   }
-  else {
-	  smallmessage[0] = TIMES_AROUND_LOOP;
-	  for(i = 1; i < ARRAY_LEN; i++)
-		  smallmessage[i] = 0;
-  }
-
-#ifdef DEBUG
-  printf("%d: About to rank-0 send....\n", rank);
-#endif
-
-  if(rank == 0)
-  {
-    // sf:  adding sleep to fix lack of barrier
-    usleep(500000);
-    MPI_Send(smallmessage, ARRAY_LEN, MPI_INT, (rank+1)%size, TAG, MPI_COMM_WORLD); /* inject initial message to ring  */
-  }
-
-#ifdef DEBUG
-  printf("%d: Entering do loop...\n",rank);
-#endif
-
-  while (1)
-  {
-#ifdef DEBUG
-    printf("%d: About to MPI_Recv...\n", rank);
-#endif
-
-    MPI_Recv(smallmessage, ARRAY_LEN, MPI_INT, (rank-1+size)%size, TAG, MPI_COMM_WORLD, &status);
-#ifdef DEBUG
-    printf("%d:  smallmessage[0] is now %d\n", rank, smallmessage[0]);
-#endif
-
-    // Checkpoint after completion of each ring
-    // Exit process 0 after completion of 5 ring
-    if ((smallmessage[0] == process_0_exit) && rank == 0) {
-    	--smallmessage[0];
-    	MPI_Checkpoint(&epoch);
-    	Application_Checkpoint_save(epoch, smallmessage);
-    	exit(123);
-    }
-    else if(rank == 0) {
-      --smallmessage[0];
-      MPI_Checkpoint(&epoch);
-      Application_Checkpoint_save(epoch, smallmessage);
-    }
-
-#ifdef DEBUG
-    printf("%d: About to MPI_Send...\n", rank);
-#endif
-
-    MPI_Send(smallmessage, ARRAY_LEN, MPI_INT, (rank+1)%size, TAG, MPI_COMM_WORLD); /* forward around the logical ring */
-
-    if (smallmessage[0] == 0) {
-#ifdef DEBUG
-    	printf("%d: Exiting loop\n", rank);
-#endif
-    	break;
-    }
-
-  }
-  if (rank == 0) {
-	  MPI_Recv(smallmessage, ARRAY_LEN, MPI_INT, (rank-1+size)%size, TAG, MPI_COMM_WORLD, &status);
-	  #ifdef DEBUG
-	      printf("%d:  smallmessage[0] is now %d\n", rank, smallmessage[0]);
-	  #endif
-  }
-
-#ifdef DEBUG
-  printf("%d: Exiting\n", rank);
-#endif
-
-  MPI_Finalize();
+  code = MPI_Finalize();
   return 0;
+}
+
+int main_loop(int epoch, int *done) {
+	int i;
+	int code = MPI_SUCCESS;
+	int rank, size;
+	int smallmessage[ARRAY_LEN];
+	MPI_Status status;
+
+	MPIX_SAFE_CALL(MPI_Comm_size(MPI_COMM_WORLD, &size), code = MPIX_TRY_RELOAD, fail_return);
+	MPIX_SAFE_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank), code = MPIX_TRY_RELOAD, fail_return);
+
+	printf("%d: Size=%d, rank=%d\n", rank, size, rank);
+
+	//MPIX_SAFE_CALL(MPI_Barrier(MPI_COMM_WORLD), code = MPIX_TRY_RELOAD, fail_return);
+
+	if (epoch > 0) {
+		Application_Checkpint_Read(epoch, smallmessage);
+	}
+	else {
+		smallmessage[0] = TIMES_AROUND_LOOP;
+		for (i = 1; i < ARRAY_LENl i++) {
+			smallmessage[i] = 0;
+		}
+	}
+	if (rank == 0) {
+		MPIX_SAFE_CALL(MPI_Send(smallmessage, ARRAY_LEN, MPI_Init, (rank + 1) % size, TAG, MPI_COMM_WORLD), code = MPIX_TRY_RELOAD, fail_return);
+	}
+
+	while (1) {
+		printf("%d: About to MPI_Recv...\n", rank);
+		MPIX_SAFE_CALL(MPI_Recv(smallmessage, ARRAY_LEN, MPI_Init, (rank - 1 + size) % size, TAG, MPI_COMM_WORLD, &status), code = MPIX_TRY_RELOAD, fail_return);
+		printf("%d:  smallmessage[0] is now %d\n", rank, smallmessage[0]);
+
+		if (rank == 0) {
+			Application_Checkpoint_Write(epoch, smallmessage);
+			MPIX_SAFE_CALL(MPIX_Checkpoint(&epoch), code = MPIX_TRY_RELOAD, fail_return);
+			--smallmessage[0];
+		}
+		printf("%d: About to MPI_Send...\n", rank);
+
+		MPIX_SAFE_CALL(MPI_Send(smallmessage, ARRAY_LEN, MPI_Init, (rank + 1) % size, TAG, MPI_COMM_WORLD), code = MPIX_TRY_RELOAD, fail_return);
+
+		if (smallmessage[0] == 0) {
+			break;
+		}
+	}
+	if (rank == 0) {
+		MPIX_SAFE_CALL(MPI_Recv(smallmessage, ARRAY_LEN, MPI_Init, (rank - 1 + size) % size, TAG, MPI_COMM_WORLD, &status), code = MPIX_TRY_RELOAD, fail_return);
+		printf("%d:  smallmessage[0] is now %d\n", rank, smallmessage[0]);
+	}
+	printf("%d: Exiting\n", rank);
+
+	if (code == MPI_SUCCESS) {
+		*done = 1;
+	}
+
+	return code;
+
+	fail_return:
+	return MPIX_TRY_RELOAD;
 }
