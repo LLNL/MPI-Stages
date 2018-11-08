9866ed26ed5a9890c4a8e85108fbf0156668dff5
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2018-11-08 16:38:42 -0500

    Added errors.h for MPI error codes

---
+++ include/errors.h
@@ -0,0 +1,14 @@
+#ifndef __EXAMPI_ERRORS_H
+#define __EXAMPI_ERRORS_H
+
+enum mpi_error 
+{
+	MPI_SUCCESS = 0,
+	MPI_ERR_COMM,
+	MPI_ERR_COUNT,
+	MPI_ERR_TYPE,
+	MPI_ERR_TAG,
+	MPI_ERR_RANK
+};
+
+#endif
