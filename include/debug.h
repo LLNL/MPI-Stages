e82b483693b9f5b0cee1291fc0d69c42fa7c970d
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2018-12-12 09:46:54 -0500

    Improved debug output slightly

--- include/debug.h
+++ include/debug.h
@@ -6,7 +6,6 @@
 
 namespace exampi {
 
-#ifdef DEBUG
 // debug print mutex, so threads don't overwrite each others output
 // this makes debug really slow
 extern std::mutex debug_mutex;
@@ -16,7 +15,13 @@ std::string debug_init();
 
 // TODO rename this to debug once everything is wrapped, then sed to global replace
 //#define debugpp(msg) std::clog << debug_init() << msg << std::endl;
-#define debugpp(msg) debug_mutex.lock(); std::clog << msg << std::endl << std::flush; debug_mutex.unlock();
+
+#ifdef DEBUG
+
+#define debug_function_init(name) 
+#define debug_function_exit(name) 
+
+#define debugpp(msg) debug_mutex.lock(); std::clog << debug_init() << msg << std::endl << std::flush; debug_mutex.unlock();
 
 #else
 
--- src/debug.cpp
+++ src/debug.cpp
@@ -1,23 +1,24 @@
 #include "debug.h"
 
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <thread>
+#include <map>
 
 namespace exampi {
 
-// TODO refactor/redo this indentation depending on thread and level?
-// level would be very nice, T3DMPI
-std::string debut_init()
+std::string debug_init()
 {
-	return std::string("");
+	std::stringstream stream;
+
+	// TODO improve thread naming, hex is ugly and hard to read
+	stream << "[0x" << std::hex << std::setfill('0') << std::setw(8) << std::this_thread::get_id() << "] ";
+
+	// lookout into nice name table
+
+	return stream.str();
 }
-//{
-//	std::stringstream stream;
-//
-//	//debug_stringstream << "\t[0x" << std::hex << std::setfill('0') << std::setw(
-//	//                       8) << thisThread() << "] ";
-//
-//	return stream.str();
-//}
 
 
 std::mutex debug_mutex;
