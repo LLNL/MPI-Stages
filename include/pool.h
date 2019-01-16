2c5939d2fc0f0285609cf328fdc50973597fbffb
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2019-01-16 11:25:04 -0500

    Added uniqueptr request memory pool

--- include/pool.h
+++ include/pool.h
@@ -12,21 +12,22 @@
 namespace exampi
 {
 
-template <typename T> struct minipool
+template <typename T> 
+class MemoryPool
 {
-	union minipool_item
+	union MemoryPool_item
 	{
-private:
+	private:
 		using StorageType = alignas(alignof(T)) char[sizeof(T)];
 
-		minipool_item *next;
+		MemoryPool_item *next;
 		StorageType datum;
-public:
-		minipool_item *get_next_item() const
+	public:
+		MemoryPool_item *get_next_item() const
 		{
 			return next;
 		}
-		void set_next_item(minipool_item *n)
+		void set_next_item(MemoryPool_item *n)
 		{
 			next = n;
 		}
@@ -36,21 +37,21 @@ public:
 			return reinterpret_cast<T *>(datum);
 		}
 
-		static minipool_item *storage_to_item(T *t)
+		static MemoryPool_item *storage_to_item(T *t)
 		{
-			minipool_item *current_item = reinterpret_cast<minipool_item *>(t);
+			MemoryPool_item *current_item = reinterpret_cast<MemoryPool_item *>(t);
 			return current_item;
 		}
 	};
 
-	struct minipool_arena
+	struct MemoryPool_arena
 	{
 	private:
-		std::unique_ptr<minipool_item[]> storage;
-		std::unique_ptr<minipool_arena> next;
+		std::unique_ptr<MemoryPool_item[]> storage;
+		std::unique_ptr<MemoryPool_arena> next;
 
 	public:
-		minipool_arena(size_t arena_size) : storage(new minipool_item[arena_size])
+		MemoryPool_arena(size_t arena_size) : storage(new MemoryPool_item[arena_size])
 		{
 			for (size_t i = 1; i < arena_size; i++)
 			{
@@ -59,12 +60,12 @@ public:
 			storage[arena_size - 1].set_next_item(nullptr);
 		}
 
-		minipool_item *get_storage() const
+		MemoryPool_item *get_storage() const
 		{
 			return storage.get();
 		}
 
-		void set_next_arena(std::unique_ptr<minipool_arena> &&n)
+		void set_next_arena(std::unique_ptr<MemoryPool_arena> &&n)
 		{
 			assert(!next);
 
@@ -73,47 +74,19 @@ public:
 	};
 
 	size_t arena_size;
-	std::unique_ptr<minipool_arena> arena;
-	minipool_item *free_list;
+
+	std::unique_ptr<MemoryPool_arena> arena;
+	MemoryPool_item *free_list;
+
 	std::mutex sharedlock;
 
-	minipool(size_t arena_size)
-		: arena_size(arena_size), arena(new minipool_arena(arena_size)),
-		  free_list(arena->get_storage()) {}
+public:
+	MemoryPool(size_t arena_size);
 
-	template <typename... Args> T *alloc(Args &&... args)
-	{
-		std::lock_guard<std::mutex> lock(sharedlock);
+	template <typename... Args> 
+	std::unique_ptr<T> alloc(Args &&... args);
 
-		if (free_list == nullptr)
-		{
-			std::unique_ptr<minipool_arena> new_arena(new minipool_arena(arena_size));
-
-			new_arena->set_next_arena(std::move(arena));
-			arena.reset(new_arena.release());
-			free_list = arena->get_storage();
-		}
-
-		minipool_item *current_item = free_list;
-		free_list = current_item->get_next_item();
-
-		T *result = current_item->get_storage();
-		new (result) T(std::forward<Args>(args)...);
-
-		return result;
-	}
-
-	void free(T *t)
-	{
-		std::lock_guard<std::mutex> lock(sharedlock);
-
-		t->T::~T();
-
-		minipool_item *current_item = minipool_item::storage_to_item(t);
-
-		current_item->set_next_item(free_list);
-		free_list = current_item;
-	}
+	void free(std::unique_ptr<T> t);
 };
 
 }
