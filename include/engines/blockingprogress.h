e3e2b5da604c915eba7b490799c1ab6b2df786d9
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2019-01-28 15:10:03 -0500

    Initial BlockingProgress work.

--- include/engines/blockingprogress.h
+++ include/engines/blockingprogress.h
@@ -1,6 +1,8 @@
 #ifndef __EXAMPI_BLOCKING_PROGRESS_H
 #define __EXAMPI_BLOCKING_PROGRESS_H
 
+#include <thread>
+
 #include "abstract/progress.h"
 
 namespace exampi
@@ -9,10 +11,14 @@ namespace exampi
 class BlockingProgress: public Progress
 {
 private:
-	
+	std::vector<std::thread> progress_threads;
 	
 public:
-	
+	BlockingProgress();
+	~BlockingProgress();	
+
+	postSend();
+	postRecv();
 };
 
 }
