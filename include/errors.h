9ea115728d10015fb266106d9069e401692254ca
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2019-01-30 15:39:29 -0500

    Added user input sanitizing

--- include/errors.h
+++ include/errors.h
@@ -14,6 +14,34 @@ enum mpi_error
 	MPI_ERR_RANK
 };
 
+#ifdef RUNTIME_ARGUMENT_CHECK
+
+// TODO debugpp("santizing buffer"); if...
+
+#define CHECK_BUFFER(buffer)
+#define CHECK_COMM(communicator) 
+#define CHECK_COUNT(count) 
+#define CHECK_TAG(tag) 
+#define CHECK_DATATYPE(datatype) 
+#define CHECK_RANK(rank)
+
+#define CHECK_REQUEST(request) 
+#define CHECK_STATUS(status) 
+
+#else
+
+#define CHECK_BUFFER(buffer)
+#define CHECK_COMM(communicator) 
+#define CHECK_COUNT(count) 
+#define CHECK_TAG(tag) 
+#define CHECK_DATATYPE(datatype) 
+#define CHECK_RANK(rank)
+
+#define CHECK_REQUEST(request) 
+#define CHECK_STATUS(status) 
+
+#endif
+
 }
 
 #endif
