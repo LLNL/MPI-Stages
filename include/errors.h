f7639fd525d8e44ec04ba4f20787a86121f5f92c
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2019-01-30 16:12:22 -0500

    Handled argument errors

--- include/errors.h
+++ include/errors.h
@@ -18,12 +18,12 @@ enum mpi_error
 
 // TODO debugpp("santizing buffer"); if...
 
-#define CHECK_BUFFER(buffer)
-#define CHECK_COMM(communicator) 
-#define CHECK_COUNT(count) 
-#define CHECK_TAG(tag) 
-#define CHECK_DATATYPE(datatype) 
-#define CHECK_RANK(rank)
+#define CHECK_BUFFER(buffer)			if(check_buffer(buffer) != 0)		return MPI_ERR_BUF; 
+#define CHECK_COMM(communicator)		if(check_comm(communicator) != 0) 	return MPI_ERR_COMM; 
+#define CHECK_COUNT(count)				if(check_count(count) != 0) 		return MPI_ERR_COUNT; 
+#define CHECK_TAG(tag) 					if(check_tag(tag) != 0) 			return MPI_ERR_TAG;
+#define CHECK_DATATYPE(datatype)		if(check_datatype(datatype) != 0) 	return MPI_ERR_TYPE; 
+#define CHECK_RANK(rank)				if(check_rank(rank) != 0) 			return MPI_ERR_RANK;
 
 #define CHECK_REQUEST(request) 
 #define CHECK_STATUS(status) 
