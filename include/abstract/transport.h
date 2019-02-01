ecffbc463d5cb13b7b3e269497e60b1ddad8c5b9
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2019-02-01 13:12:27 -0500

    Rewrote Transport interface

--- include/abstract/transport.h
+++ include/abstract/transport.h
@@ -1,33 +1,51 @@
 #ifndef __EXAMPI_I_TRANSPORT_H
 #define __EXAMPI_I_TRANSPORT_H
 
-#include <mpi.h>
-#include <protocol.h>
+#include <mutex>
+
+#include "mpi.h"
+#include "protocol.h"
+#include "pool.h"
 
 namespace exampi
 {
 
 class Transport
 {
-public:
-	// remove these RAII
-	virtual void init() = 0;
-	virtual void finalize() = 0;
+private:
+	// transport owns all protocol messages
+	MemoryPool<ProtocolMessage> protocol_message_pool;
 
-	// OLD
-	//virtual std::future<int> send(std::vector<struct iovec> &iov, int dest,
-	//                              MPI_Comm comm) = 0;
-	//virtual std::future<int> receive(std::vector<struct iovec> &iov, MPI_Comm comm,
-	//                                 ssize_t *count) = 0;
-	//virtual int peek(std::vector<struct iovec> &iov, MPI_Comm comm) = 0;
-	
+public:
+	// TODO find solution for settable pool size
+	Transport() : protocol_message_pool(128);
+	{
+		;
+	}
+
+	std::unique_ptr<ProtocolMessage> allocate_protocol_message()
+	{
+		return protocol_message_pool.allocate();
+	}
+
 	// stages
 	virtual int save(std::ostream &r) = 0;
 	virtual int load(std::istream &r) = 0;
 	virtual int cleanUp(MPI_Comm comm) = 0;
 
 	virtual std::unique_ptr<ProtocolMessage> absorb() = 0;
-	virtual int send(std::unique_ptr<ProtocolMessage> message) = 0;
+	//{
+	//	std::lock_guard<std::mutex> lock(guard);
+	//	if(protocol_queue.size() == 0)
+	//		return std::unique_ptr<ProtocolMessage>(nullptr);
+	//	
+	//	std::unique_ptr<ProtocolMessage> msg = protocol_queue.front()
+	//	protocol_queue.pop();
+
+	//	return msg;
+	//}
+
+	virtual int reliable_send(std::unique_ptr<ProtocolMessage> message) = 0;
 };
 
 } // ::exampi
