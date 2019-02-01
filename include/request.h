edef8f552ebabbbcabb445b2ea73f6c18fa11655
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2019-02-01 11:02:07 -0500

    Modified request to be interface between MPI interface and progress engine

--- include/request.h
+++ include/request.h
@@ -4,68 +4,48 @@
 #include <mutex>
 #include <condition_variable>
 
-#include "datatype.h"
 #include "mpi.h"
+#include "envelope.h"
+#include "payload.h"
 
 namespace exampi
 {
 
-enum class Op: int
+enum class Operation: int
 {
 	Send,
+	Ssend,
+	Bsend,
+	Rsend,
+
 	Receive
+
+	//AllReduce
+	//AllGather
+	//...
 };
 
-// 
-//extern thread_local std::mutex thr_request_lock;
+// thread waiting mechanism
 extern thread_local std::condition_variable thr_request_condition;
 
 struct Request
 {
+	// management data
 	std::mutex lock;
 	std::condition_variable *condition;
 
-	volatile bool complete; // if true no longer in protocol queue
+	bool complete;
 	bool cancelled;
+
 	bool persistent;
 	bool active;
 
-	Op op;
-	int tag;
-	int source;
-	int destination;
-	MPI_Comm communicator;
-	const void *buffer;
-	Datatype datatype;
-	int count;
-	int stage;
+	// MPI data
+	Operation operation;
+	Envelope envelope;
+	Payload payload;
 
-	Request();
-
-	//void pack()
-	//{
-	//	uint16_t *word = (uint16_t *) hdr;
-	//	uint32_t *dword = (uint32_t *) hdr;
-	//	word[0] = 0xDEAF; // magic word
-	//	word[1] = 22;  // protocol
-	//	word[2] = 42;  // message type
-	//	word[3] = 0x0; // function
-	//	dword[2] = 0x0;  // align
-	//	dword[3] = stage;  // align/reserved
-	//	dword[4] = source;
-	//	dword[5] = tag;
-	//	dword[6] = comm; // context; not yet
-	//	dword[7] = 0xAABBCCDD;  // CRC
-	//}
-
-	//void unpack()
-	//{
-	//	uint32_t *dword = (uint32_t *) hdr;
-	//	stage = dword[3];
-	//	source = dword[4];
-	//	tag = dword[5];
-	//	comm = dword[6];
-	//}
+	Request(Operation operation, Payload payload, Envelope envelope);
 };
 
 }
