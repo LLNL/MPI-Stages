ec8106faeb68ef1e53dd3bcb8b164e4c0f0ccd2a
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2019-02-14 16:00:10 -0500

    Added SimpleDecider

--- include/abstract/decider.h
+++ include/abstract/decider.h
@@ -2,13 +2,15 @@
 #define __EXAMPI_DECIDER_H
 
 #include "protocol.h"
+#include "universe.h"
+#include "request.h"
 
 namespace exampi
 {
 
 struct Decider
 {
-	Protocol decide(const Request_ptr request, const Universe &universe) const = 0;
+	virtual Protocol decide(const Request_ptr request, const Universe &universe) const = 0;
 };
 
 }
