2c1160da46e8a7ae7d45a55bd17e1a552267d775
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2019-02-19 18:17:20 -0500

    Converted header* to unique_ptr in transports

--- include/abstract/transport.h
+++ include/abstract/transport.h
@@ -1,9 +1,9 @@
 #ifndef __EXAMPI_INTERFACE_TRANSPORT_H
 #define __EXAMPI_INTERFACE_TRANSPORT_H
 
-// todo remove with mpi stages
 #include <fstream>
 #include <map>
+#include <memory>
 
 #include "header.h"
 #include "request.h"
@@ -21,19 +21,16 @@ struct Transport
 	virtual int load(std::istream &r) = 0;
 	virtual int halt() = 0;
 
-	// never called
-	//virtual int cleanUp(MPI_Comm comm) = 0;
-
 	// ordered map (preference) of protocol initator and maximum message size
 	// note chose size_t over long int, because -1 == inf would work
 	//      but it cuts down by a large range, max size_t is enough
 	virtual const std::map<Protocol, size_t> &provided_protocols() const = 0;
 
 	// receive a header or nullptr
-	virtual Header *ordered_recv() = 0;
+	virtual std::unique_ptr<Header> ordered_recv() = 0;
 
 	// fill header into matching request
-	virtual void fill(const Header *, Request *) = 0;
+	virtual void fill(std::unqiue_ptr<Header>, Request *) = 0;
 
 	// send request with protocol
 	virtual void reliable_send(const Protocol, const Request *) = 0;
