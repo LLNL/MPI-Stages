a87a096f802f7b78c9c4c2e57aab03d04e4c7643
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2019-03-18 13:02:58 -0400

    Refactored communicator

--- include/communicator.h
+++ include/communicator.h
@@ -10,106 +10,33 @@ namespace exampi
 
 struct Comm
 {
-	Comm()
-	{
-	}
+	Comm();
 	Comm(bool _isintra, std::shared_ptr<Group> _local,
-	     std::shared_ptr<Group> _remote) :
-		is_intra(_isintra), local(_local), remote(_remote)
-	{
-	}
+	     std::shared_ptr<Group> _remote);
 
-	virtual ~Comm()
-	{
-	}
+	virtual ~Comm();
 
 	// fix rule of 5 later
 	// Comm &operator=(Comm &rhs) = 0;
-	int get_next_context(int *pt2pt, int *coll)
-	{
-		int rc;
+	int get_next_context(int *pt2pt, int *coll);
 
-		if (rank == 0)
-		{
-			//Context::contextLock.lock();
-
-			Context::nextID++;
-			*pt2pt = Context::nextID;
-			Context::nextID++;
-			*coll = Context::nextID;
-
-			//Context::contextLock.unlock();
-		}
-
-		rc = MPI_Bcast(pt2pt, 1, MPI_INT, 0, local_pt2pt);
-		if (rc != MPI_SUCCESS)
-		{
-			return MPIX_TRY_RELOAD;
-		}
-
-		rc = MPI_Bcast(coll, 1, MPI_INT, 0, local_pt2pt);
-		if (rc != MPI_SUCCESS)
-		{
-			return MPIX_TRY_RELOAD;
-		}
-
-		return 0;
-	}
 	// accessors
-	std::shared_ptr<Group> get_local_group()
-	{
-		return local;
-	}
-	std::shared_ptr<Group> get_remote_group()
-	{
-		return remote;
-	}
+	std::shared_ptr<Group> get_local_group();
+	std::shared_ptr<Group> get_remote_group();
 
-	void set_local_group(std::shared_ptr<Group> group)
-	{
-		local = group;
-	}
+	void set_local_group(std::shared_ptr<Group> group);
+	void set_remote_group(std::shared_ptr<Group> group);
 
-	void set_remote_group(std::shared_ptr<Group> group)
-	{
-		remote = group;
-	}
+	int get_context_id_pt2pt() const;
+	int get_context_id_coll() const;
 
-	int get_context_id_pt2pt() const
-	{
-		return local_pt2pt;
-	}
+	void set_rank(int r);
+	void set_context(int pt2pt, int coll);
 
-	int get_context_id_coll() const
-	{
-		return local_coll;
-	}
+	int get_rank();
 
-	void set_rank(int r)
-	{
-		rank = r;
-	}
-
-	void set_context(int pt2pt, int coll)
-	{
-		local_pt2pt = pt2pt;
-		local_coll = coll;
-	}
-
-	int get_rank()
-	{
-		return rank;
-	}
-
-	bool get_is_intra()
-	{
-		return is_intra;
-	}
-
-	void set_is_intra(bool intra)
-	{
-		is_intra = intra;
-	}
+	bool get_is_intra();
+	void set_is_intra(bool intra);
 
 	// Nawrin task for later, introduce the entire MPI API here as methods of this comm; right now, we do "Shane-mode,"
 	// where the C API directly calls the Interface singleton, which is allowed to use accessors of Comm for info.
---
+++ src/communicator.cc
@@ -0,0 +1,111 @@
+#include "communicator.h"
+
+#include "mpi.h"
+
+namespace exampi
+{
+
+Comm::Comm()
+{
+}
+
+Comm::Comm(bool _isintra, std::shared_ptr<Group> _local,
+	 std::shared_ptr<Group> _remote) :
+	is_intra(_isintra), local(_local), remote(_remote)
+{
+}
+
+Comm::~Comm()
+{
+}
+
+// fix rule of 5 later
+// Comm &operator=(Comm &rhs) = 0;
+int Comm::get_next_context(int *pt2pt, int *coll)
+{
+	int rc;
+
+	if (rank == 0)
+	{
+		//Context::contextLock.lock();
+
+		Context::nextID++;
+		*pt2pt = Context::nextID;
+		Context::nextID++;
+		*coll = Context::nextID;
+
+		//Context::contextLock.unlock();
+	}
+
+	rc = MPI_Bcast(pt2pt, 1, MPI_INT, 0, local_pt2pt);
+	if (rc != MPI_SUCCESS)
+	{
+		return MPIX_TRY_RELOAD;
+	}
+
+	rc = MPI_Bcast(coll, 1, MPI_INT, 0, local_pt2pt);
+	if (rc != MPI_SUCCESS)
+	{
+		return MPIX_TRY_RELOAD;
+	}
+
+	return 0;
+}
+
+// accessors
+std::shared_ptr<Group> Comm::get_local_group()
+{
+	return local;
+}
+std::shared_ptr<Group> Comm::get_remote_group()
+{
+	return remote;
+}
+
+void Comm::set_local_group(std::shared_ptr<Group> group)
+{
+	local = group;
+}
+
+void Comm::set_remote_group(std::shared_ptr<Group> group)
+{
+	remote = group;
+}
+
+int Comm::get_context_id_pt2pt() const
+{
+	return local_pt2pt;
+}
+
+int Comm::get_context_id_coll() const
+{
+	return local_coll;
+}
+
+void Comm::set_rank(int r)
+{
+	rank = r;
+}
+
+void Comm::set_context(int pt2pt, int coll)
+{
+	local_pt2pt = pt2pt;
+	local_coll = coll;
+}
+
+int Comm::get_rank()
+{
+	return rank;
+}
+
+bool Comm::get_is_intra()
+{
+	return is_intra;
+}
+
+void Comm::set_is_intra(bool intra)
+{
+	is_intra = intra;
+}
+
+}
