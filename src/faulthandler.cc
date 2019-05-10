dbd06f5286e40984e5b336c812d0dce0e3280578
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2019-05-10 17:05:30 -0400

    Added header to all src/ and include/ files.

--- src/faulthandler.cc
+++ src/faulthandler.cc
@@ -1,3 +1,14 @@
+// 
+// This is a source code component of ExaMPI, a next-generation 
+// implementation of the Message Passing Interface.
+//
+// This is a joint project of University of Tennessee at Chattanooga,
+// the Lawrence Livermore National Laboratory, and Auburn University.
+//
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+// 
+
 #include <iostream>
 
 #include "faulthandler.h"
