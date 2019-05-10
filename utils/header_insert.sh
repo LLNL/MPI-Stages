0e4fe27ab726c8617471177afdf55df2a1bc4d48
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2019-05-10 17:05:08 -0400

    Modified header insert script to only place header into src/ and include/

--- utils/header_insert.sh
+++ utils/header_insert.sh
@@ -2,7 +2,12 @@
 
 shopt -s globstar nullglob extglob
 
-for file in **/*.@(cc|cpp|h|hpp); do
+for file in include/**/*.@(cc|cpp|h|hpp); do
+	cat utils/header.txt $file > $file.new
+	mv $file.new $file
+done
+
+for file in src/**/*.@(cc|cpp|h|hpp); do
 	cat utils/header.txt $file > $file.new
 	mv $file.new $file
 done
